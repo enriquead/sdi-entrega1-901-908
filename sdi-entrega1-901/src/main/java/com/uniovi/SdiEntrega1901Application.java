124650ed12c12441de78f666867a521a01252fc7
Enrique Avello Díaz <uo258119@uniovi.es>
2019-02-25 10:24:17 +0100

    First commit

---
+++ sdi-entrega1-901/src/main/java/com/uniovi/SdiEntrega1901Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega1901Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega1901Application.class, args);
+	}
+
+}
