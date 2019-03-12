d2e483ffb4e0e816431c3a62a201adc9ee628f12
Sofía Martín <sofimrtn@gmail.com>
2019-03-12 14:13:31 +0100

    Añadida fecha a las ofertas (calendario gregoriano)

--- sdi-entrega1-901/src/main/java/com/uniovi/entities/Offer.java
+++ sdi-entrega1-901/src/main/java/com/uniovi/entities/Offer.java
@@ -11,7 +11,7 @@ public class Offer {
 	private Long id;
 	private String title;
 	private String details;
-	private Date upDate;
+	private String upDate;
 	private double price;
 	
 	@ManyToOne
@@ -39,12 +39,13 @@ public class Offer {
 		this.user = user;
 	}
 	
-	public Offer(String title, String details, Date upDate, double price) {
+	public Offer(String title, String details, String upDate, double price, User user) {
 		super();
 		this.title = title;
 		this.details = details;
 		this.upDate = upDate;
 		this.price = price;
+		this.user=user;
 	}
 
 	public Long getId() {
@@ -71,11 +72,11 @@ public class Offer {
 		this.details = details;
 	}
 
-	public Date getUpDate() {
+	public String getUpDate() {
 		return upDate;
 	}
 
-	public void setUpDate(Date upDate) {
+	public void setUpDate(String upDate) {
 		this.upDate = upDate;
 	}
 
